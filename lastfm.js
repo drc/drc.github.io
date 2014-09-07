d661e1e766731d52fa6be3b131404f6a0a994326
dancigrang <danielcigrang@me.com>
2014-09-07 00:51:26 -0500

    more css

--- lastfm.js
+++ lastfm.js
@@ -22,13 +22,14 @@ var getRecent = $.ajax({
 getRecent.done(function(data) {
     console.log('Current artist: ', data.recenttracks.track[0].artist['#text']);
     console.log('Current album: ', data.recenttracks.track[0].album['#text']);
+    console.log(data);
     $.ajax({
         type: "GET",
         url: "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" + key + "&artist=" + data.recenttracks.track[0].artist['#text'] + "&album=" + data.recenttracks.track[0].album['#text'] + "&format=json"
     })
         .done(
             function(data) {
-                //console.log(data.album.image[4]);
+                //console.log(data);
                 console.log((typeof data.album.image[4] === "undefined") ? "No album link on Last.fm." : data.album.image[4]['#text']);
                 document.getElementById("bg").style.backgroundImage = "url('" + data.album.image[4]['#text'] + "')";
             })
