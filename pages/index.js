df306af9946c3dce1f788b55780652ebe1b6fc4f
Dan Cigrang <danielcigrang@me.com>
2021-09-01 15:03:31 -0500

    bump next

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Container from "@/components/Container"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Homepage for Dan Cigrang" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h2>Updates coming soon</h2>
-        <p>Placeholder for new content eventually.</p>
-      </main>
-    </div>
-  )
+    return (
+        <Container>
+            <h2>Updates coming soon</h2>
+            <p>Placeholder for new content eventually.</p>
+        </Container>
+    );
 }
